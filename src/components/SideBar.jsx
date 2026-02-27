65ade304bffbd7e41cc2dbb96069c2a3d72133de
P Anand Kumar <anandkumarp093@gmail.com>
2026-02-27 22:48:00 +0530

    Initial Commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideBar = ({selectedUser, setSelectedUser}) => {
+  return (
+    <div className='pb-5'>
+      <div className='flex justify-between items-center'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
